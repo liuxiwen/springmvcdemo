b7529222de5361b56e314974685988fd32b2a413
liuxiwen <xiwenhehe@sina.com>
2017-09-13 21:56:22 +0800

    博客Repository

---
+++ src/main/java/com/gaussic/repository/BlogRepository.java
@@ -0,0 +1,9 @@
+package com.gaussic.repository;
+
+import com.gaussic.model.BlogEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogRepository extends JpaRepository<BlogEntity, Integer> {
+}
